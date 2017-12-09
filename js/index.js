7319dcf426f64de44226d40ee1849ee129c98254
Giovana <giovanamoraess18@gmai.com>
2017-12-09 10:45:22 -0200

    chamadas login e cadastro

--- js/index.js
+++ js/index.js
@@ -23,9 +23,32 @@ function validaLogin(){
     success: function( data ){
         retorno = data;
         infosUsuario = retorno;
-    
     }
 });
 }
 
+function cadastrarUsuario(){
+    var nome = $('#idNomeCadastro').val();
+    var email = $('#idEmailCadastro').val();
+    var cpf = $('#idCPFCadastro').val();
+    var telefone = $('#idTelefoneCadastro').val();
+    var senha = $('#idSenhaCadastro').val();
 
+    var data = {
+        "email": email,
+        "senha": senha,
+        "nome" : nome,
+        "telefone": telefone
+    }
+
+  $.ajax({
+    url:    'http://localhost:52926/api/cliente/',
+    type:   "post",
+    dataType:"json",
+    data:  data,
+    async: false,
+    success: function( data ){
+        console.log('retorno cadastro', data)
+    }
+});
+}
