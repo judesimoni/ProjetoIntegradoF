7525d4e750bbbde1ba7a0237a71a61abfe8fef80
Giovana <giovanamoraess18@gmai.com>
2017-12-11 21:23:21 -0200

    finalizar compra e sweet alert

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,5 +3,27 @@ function AddCarrinho(produto, qtd, valor, posicao){
 	localStorage.setItem("qtd" + posicao, qtd);
 	valor = valor * qtd;
 	localStorage.setItem("valor" + posicao, valor);
-	alert("Produto adicionado ao carrinho!");
+	swal("Sucesso!", "Pedido adicionado ao carrinho!", "success");
+ }
+
+ function finalizarCompra() {
+
+	var rua = $('#idRua').val();
+	var bairro = $('#idBairro').val();
+	var numero = $('#idNumero').val();
+	var cep = $('#idCEP').val();
+
+
+	var nomeImpresso = $('#idNomeImpresso').val();
+	var numeroCartao = $('#idNumeroCartao').val();
+	var validade = $('#idValidade').val();
+	var codigo = $('#idCodigo').val();
+
+	if (!rua || !bairro || !numero || !cep) {
+		swal("Erro!", "Verifique o endereço digitado", "error");
+	}
+	else if (!nomeImpresso || !numeroCartao || !validade || !codigo) {
+		swal("Erro!", "Verifique as informações do cartão", "error");
+	} else 
+	swal("Sucesso!", "Compra finalizada com sucesso!", "success");
  }
